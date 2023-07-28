eb7689a22ecf9f5160319884a3cc1a50ba1f8642
H342705 <136451582+H342705@users.noreply.github.com>
2023-07-28 15:08:37 +0500

    Add files via upload

---
+++ question7.ts
@@ -0,0 +1,11 @@
+let add = 4 + 4;
+let diff = 12 - 4;
+let Multiplication = 2 * 4;
+let Division = 64 / 8;
+console.log(add);
+console.log(diff);
+console.log(Multiplication);
+console.log(Division);
+
+
+
