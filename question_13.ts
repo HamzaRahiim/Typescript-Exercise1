ac78d24e807eb10ff9bd774229b53fa852cb1cfe
Hamza Rahim <136451582+H342705@users.noreply.github.com>
2023-07-30 15:19:56 +0500

    Add files via upload

---
+++ question_13.ts
@@ -0,0 +1,6 @@
+let transport = ["Fighterjet.","Airplane."];
+
+    let message = "I would like to Always fly in";
+    let message2 = "My favorite transportation is"
+    console.log(`${message} ${transport}`);
+    console.log(`${message2} ${transport}`);
