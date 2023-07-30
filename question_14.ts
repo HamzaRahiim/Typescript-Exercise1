e380cf99e7a25999308965b266ba41119f3e5c6f
Hamza Rahim <136451582+H342705@users.noreply.github.com>
2023-07-30 15:20:23 +0500

    Add files via upload

---
+++ question_14.ts
@@ -0,0 +1,5 @@
+let invite = ["Umar","saad","Abubakar"];
+    for(var i=0; i < invite.length; i++){
+        let message =`Mr.${invite[i]},you are invited for Dinner.`;
+        console.log(message);
+    }
