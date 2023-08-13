2b1ddc684b5ed8c32c985b05531ce2829673d7a9
Hamza Rahim <136451582+H342705@users.noreply.github.com>
2023-08-13 15:40:13 +0500

    question3.js

--- question2.js
+++ question2.js
@@ -1,16 +1,16 @@
-var fName = "Hamza rahim";
+let fName:string = "Hamza rahim";
 console.log(fName.toLowerCase());
 console.log(fName.toUpperCase());
 //writing a program to change name to title case.
-var amer = fName.split(' ');
-var arr = [''];
-for (var i = 0; i < amer.length; i++) {
-    var string = amer[i];
-    for (var j = 0; j < string.length; j++) {
-        var call = string.charAt(0);
-        var replacement = string.replace(call, call.toUpperCase());
+let title = fName.split(' ');
+let arr:string[] = [];
+for (let i = 0; i < title.length; i++) {
+    let  string = title[i];
+    for (let j = 0; j < string.length; j++) {
+        let call = string.charAt(0);
+        let replacement = string.replace(call, call.toUpperCase());
         arr.push(replacement);
         break;
     }
 }
-console.log("".concat(arr[1], " ").concat(arr[2]));
+console.log(...arr);
