ef05d66ecdc4c1bb52e23f1f7e67bd4f43ee8459
Hamza Rahim <hamzarahim836@gmail.com>
2023-08-13 16:06:05 +0500

    question4.ts

--- question4.ts
+++ question4.ts
@@ -1,4 +1,4 @@
-let  author = "Abraham Lincolon";
-let  quote = "Whatever you are,be a good one."
+let  author:string = "Abraham Lincolon";
+let  quote:string = "Whatever you are,be a good one."
 
 console.log(`${author} once said,"${quote}"`);
